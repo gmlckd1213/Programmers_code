6e3ac92116553e708ed5a13d964f148bc1fe4f8e
gmlckd1213 <gmlckd1213@nate.com>
2022-05-21 16:21:11 +0900

    소수만들기

---
+++ Level1/소수만들기.py
@@ -0,0 +1,22 @@
+def solution(num):
+    for i in range(len(nums)):
+         for j in range(len(nums)):
+             for k in range(len(nums)):
+                if i==j or j==k or i == j == k:
+                     continue
+                else:
+                    answer = nums[i]+nums[j]+nums[k]
+                    if sosu(answer) == True:
+                        cnt += 1
+    return cnt 
+
+def sosu(n):
+    if n== 0 or n == 1:
+        return False
+    else:
+        for i in range(2,n//2+1):
+            if n%i == 0:
+                return False
+        return True
+
+                
