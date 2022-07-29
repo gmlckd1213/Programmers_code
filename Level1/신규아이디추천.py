e7f50afbccf8ae2d7816857ec222f2d1c15878cd
gmlckd1213 <gmlckd1213@nate.com>
2022-07-29 21:32:53 +0900

    로또

---
+++ Level1/신규아이디추천.py
@@ -0,0 +1,26 @@
+
+def solution(new_id):
+    answer=""
+    new_id = new_id.lower()
+    for value in new_id:
+        if value.isdigit() or value.islower() or value in ['-','_','.']:
+            answer += value
+    while '..' in answer:
+        answer=answer.replace("..",'.')
+    if answer[0] == '.':
+        if len(answer) >= 2:
+            answer=answer[1:]
+        else:
+            answer="."
+    if answer[-1] == '.':
+        answer=answer[:-1]
+        
+    if answer == "":
+        answer = "a"
+    if len(answer) >= 16:
+        answer = answer[:15]
+        if answer[-1] == '.':
+            answer = answer[:-1]
+    while len(answer)<3:
+        answer += answer[-1]
+    return answer
