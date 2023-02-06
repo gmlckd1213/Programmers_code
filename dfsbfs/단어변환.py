d1c7f63b6dada7573a7e3c0406e33278de98fe7a
gmlckd1213 <gmlckd1213@nate.com>
2023-02-06 16:24:47 +0900

    ㄴ

--- dfsbfs/단어변환.py
+++ dfsbfs/단어변환.py
@@ -1,22 +1,19 @@
-def solution(begin, target, words):
-    if target not in words:  # 애초에 words리스트에 target값이 없다면 return 0
-        return 0
 
-    q = deque()
-    q.append([begin, 0])
+def visit(k, graph, visited):
+    visited[k] = 1
+    for i in range(len(graph[k])):
+        if visited[i] == 0 and graph[k][i] == 1:
+            visit(i, graph, visited)
 
-    while q:
-        x, cnt = q.popleft()
+def solution(n, computers):
 
-        if x == target:
-            return cnt # target값이 되면 cnt 출력
+    visited = [0] * n
 
-        for i in range(0, len(words)):
-            diff = 0
-            word = words[i]
-            for j in range(len(x)):
-                if x[j] != word[j]:
-                    diff += 1
-            if diff == 1:
-                q.append([word, cnt + 1])
-    return 0 # target값이 있으나 변환될 수 없다면
+    answer = 0
+
+    for i in range(n):
+        if visited[i] == 0:
+            visit(i, computers, visited)
+            answer += 1
+
+    return answer
